556c549138963503f196422fbf35f23897786535
TylereDupont <tyleredupont@gmail.com>
2021-11-09 22:02:58 -0600

    Add files via upload

---
+++ domainRecordScanner.py
@@ -0,0 +1,15 @@
+import subprocess
+
+recordTypes = ['A','AAAA','ALIAS','CNAME','MX','NS','PTR','SOA','SRV','TXT','DNSKEY','DS','NSEC','NSEC3','NSEC3PARAM','RRSIG','AFSDB','CAA','CERT','DHCID','DNAME','HINFO','HTTPS','LOC','NAPTR','RP','TLSA']
+
+target = input("Enter Domain Name ")
+
+decodedOutput = ''
+for x in recordTypes:
+	if decodedOutput == '':
+		decodedOutput = x + "\n" + subprocess.run(['dig ' + '+short ' + target + ' ' + x] , capture_output=True, shell=True).stdout.decode() + "\n"
+	else:
+		decodedOutput += x + "\n" + subprocess.run(['dig ' + '+short ' + target + ' ' + x] , capture_output=True, shell=True).stdout.decode() + "\n"
+
+print(decodedOutput)
+#GROUP4!
